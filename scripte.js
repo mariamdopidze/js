4489a56b00d6d4b3dbfaa8d682b1cfa5648bea1c
mariamdopidze <119893246+mariamdopidze@users.noreply.github.com>
2023-01-24 02:31:40 -0800

    created html js files

---
+++ scripte.js
@@ -0,0 +1,29 @@
+
+
+// #5
+const names = [ 'ana', 'nino','giorgi','lasha','mariam'];
+names.push ('lily');
+console.log(names);
+
+const lastname = names.pop();
+console.log(lastname);
+
+const firstname = names.shift();
+console.log(firstname);
+
+names.unshift ('sofi');
+console.log (names);
+
+
+//#1
+
+let days = [ 1, 5, 9, 17, 3, 23, 38, 31, 46, 52, 67, 70, 85, 97];
+for (let index=0; index < days.length; index++){console.log(days [index]);}
+
+//#2
+let week = [ 3, 6, 7, 11, 25, 31, 46, 2, 27,39];
+
+let item = 0
+while (item < week.length ) {console.log (week[item]);
+    i++ 
+}
